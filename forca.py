1ab52288d0745b2cb6d89171cca27b1da70a7c90
nunoalves79 <sdnunoalves@gmail.com>
2026-05-08 00:45:17 +0100

    Jogo com palavra secreta simplificada
    Criado o código final, com limpeza da consola e mensagens claras para o jogador
    Foi inserido o quadro final com o resumo do jogo e resultado final
    consta apenas um país na palavra secreta para conseguir fazer as simulações controladas (quando acertar e quando errar)
    De seguida será carregado na variável palavra secreta todos os países da uniõ europeia

--- forca.py
+++ forca.py
@@ -1,48 +1,59 @@
 import random
 import os
 
-# 1. Lista de palavras a escolher aleatoriamente
+# 1. Configurações gerais e variáveis
 lista_palavras = ['Portugal']
-
-# 2. Escolher uma palavra aleatória da lista
-palavra_secreta = random.choice(lista_palavras)
-
-# 3. criar a lista de letras descobertas de forma automática
+palavra_secreta = random.choice(lista_palavras).upper()
 letras_descobertas = ['_' for _ in palavra_secreta]
-
-# 4. Definir o número de vidas
 tentativas = 6
-
-# 5. Lista de letras erradas
 letras_erradas = []
+mensagem = "Boa sorte! Tenta adivinhar a palavra secreta."
 
-#6. Criar o ciclo do jogo
+# 2. Ciclo do jogo
 while tentativas > 0 and '_' in letras_descobertas:
     os.system('cls' if os.name == 'nt' else 'clear')
-    
-    print('|***||***| BEM-VINDO AO JOGO DA FORCA: VERSÃO UNIÃO EUROPEIA |***||***|')
-    print()
-    print('\n' + '='*40)
-    print(f'Palavra Secreta: {" ".join(letras_descobertas)}')
-    print(f'Vidas Restantes: {tentativas}')
-    print(f'Letras Erradas: {", ".join(letras_erradas)}')
-    
-    palpite = input('\nDigite uma letra: ').upper()
 
-    #Verifica se a letra está na palavra secreta
-    if palpite in palavra_secreta.upper():
-        print(f"Bom palpite! A Letra: '{palpite}' existe na palavra secreta.")
-        #Atualizar a letra na palavra secreta
+# 3. Quadro do jogo  
+    print('|***||***| JOGO DA FORCA: EDIÇÃO - UNIÃO EUROPEIA |***||***|')
+    print('\n')
+    print('='*60)
+    print(f'Palavra secreta: {" ".join(letras_descobertas)}')
+    print(f'Vidas restantes: {tentativas}')
+    print(f'Letras erradas: {", ".join(letras_erradas)}')
+    print(f'Estado: {mensagem}')
+    print('='*60)
+
+# 4. Registo do palpite  
+    palpite = input('Qual o seu palpite? Indique uma Letra: ').upper()
+
+    # 5. Validar repetição de letra
+    if palpite in letras_descobertas or palpite in letras_erradas:
+        mensagem = f'ATENÇÃO! A LETRA "{palpite}" JA FOI USADA!'
+        continue  # <--- ESTA LINHA É QUE TRAVA O DESCONTO DE VIDAS
+
+    # 6. Validar se foi uma letra correta ou incorreta
+    if palpite in palavra_secreta:
+        mensagem = "BOA! ACERTASTE NUMA LETRA!"
         for i in range(len(palavra_secreta)):
-            if palavra_secreta[i].upper() == palpite:
+            if palavra_secreta[i] == palpite:
                 letras_descobertas[i] = palpite
     else:
-        print(f"Oh, que pena! A Letra '{palpite}' não existe na palavra secreta. Tenta novamente!")
+        mensagem = f"OH QUE PENA! ERRASTE. A LETRA '{palpite}' NÃO PERTENCE À PALAVRA!"
         letras_erradas.append(palpite)
         tentativas -= 1
-    print('|***||***| BEM-VINDO AO JOGO DA FORCA: VERSÃO UNIÃO EUROPEIA |***||***|')
-    print()
-    print('\n' + '='*40)
-    print(f'Palavra Secreta: {" ".join(letras_descobertas)}')
-    print(f'Vidas Restantes: {tentativas}')
-    print(f'Letras Erradas: {", ".join(letras_erradas)}')
+
+# 7. Conclusão do jogo
+os.system('cls' if os.name == 'nt' else 'clear')
+print('|***||***| JOGO DA FORCA: EDIÇÃO - UNIÃO EUROPEIA |***||***|')
+print('\n' + '='*60)
+print(f'Palavra secreta: {" ".join(letras_descobertas)}')
+print(f'Vidas restantes: {tentativas}')
+print(f'Letras erradas: {", ".join(letras_erradas)}')
+print('='*60)
+
+if '_' not in letras_descobertas:
+    print(f"\n MUITO BEM. GANHASTE! A palavra secreta era: {palavra_secreta}")
+else:
+    print(f"\n QUE PENA! PERDESTE! A palavra secreta era: {palavra_secreta}")
+
+print('='*60)
