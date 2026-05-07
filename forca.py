8d8469b3d4e719d12317a0dadbd1054b923dbd54
nunoalves79 <sdnunoalves@gmail.com>
2026-05-07 22:56:44 +0100

    Atualização do jogo. Validação de palpites terminada

--- forca.py
+++ forca.py
@@ -1,9 +1,7 @@
 import random
 
 # 1. Lista de palavras a escolher aleatoriamente
-lista_palavras = ['Bulgaria', 'Chequia', 'Chipre', 'Croacia', 'Dinamarca', 'Eslovaquia', 'Eslovenia', 'Espanha',
-'Estonia', 'Finlandia', 'Franca', 'Grecia', 'Hungria', 'Irlanda', 'Italia', 'Letonia', 'Lituania', 'Luxemburgo',
-'Malta', 'Paises Baixos', 'Polonia', 'Portugal', 'Romenia', 'Suecia']
+lista_palavras = ['Portugal']
 
 # 2. Escolher uma palavra aleatória da lista
 palavra_secreta = random.choice(lista_palavras)
@@ -23,3 +21,19 @@ print('\n' + '='*40)
 print(f'Palavra Secreta: {" ".join(letras_descobertas)}')
 print(f'Vidas Restantes: {tentativas}')
 print(f'Letras Erradas: {", ".join(letras_erradas)}')
+
+#6. Criar o ciclo do jogo
+while tentativas > 0 and '_' in letras_descobertas:
+    palpite = input('\nDigite uma letra: ').upper()
+
+    #Verifica se a letra está na palavra secreta
+    if palpite in palavra_secreta.upper():
+        print(f"Bom palpite! A Letra: '{palpite}' existe na palavra secreta.")
+        #Atualizar a letra na palavra secreta
+        for i in range(len(palavra_secreta)):
+            if palavra_secreta[i].upper() == palpite:
+                letras_descobertas[i] = palpite
+    else:
+        print(f"Oh, que pena! A Letra '{palpite}' não existe na palavra secreta. Tenta novamente!")
+        letras_erradas.append(palpite)
+        tentativas -= 1
