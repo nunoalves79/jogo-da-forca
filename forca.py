6ca260ccf9ee37c0dfa76c662307df844c04725a
nunoalves79 <sdnunoalves@gmail.com>
2026-05-07 23:27:18 +0100

    Importador os

--- forca.py
+++ forca.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 # 1. Lista de palavras a escolher aleatoriamente
 lista_palavras = ['Portugal']
@@ -15,15 +16,17 @@ tentativas = 6
 # 5. Lista de letras erradas
 letras_erradas = []
 
-print('|***||***| BEM-VINDO AO JOGO DA FORCA: VERSÃO UNIÃO EUROPEIA |***||***|')
-print()
-print('\n' + '='*40)
-print(f'Palavra Secreta: {" ".join(letras_descobertas)}')
-print(f'Vidas Restantes: {tentativas}')
-print(f'Letras Erradas: {", ".join(letras_erradas)}')
-
 #6. Criar o ciclo do jogo
 while tentativas > 0 and '_' in letras_descobertas:
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
+    print('|***||***| BEM-VINDO AO JOGO DA FORCA: VERSÃO UNIÃO EUROPEIA |***||***|')
+    print()
+    print('\n' + '='*40)
+    print(f'Palavra Secreta: {" ".join(letras_descobertas)}')
+    print(f'Vidas Restantes: {tentativas}')
+    print(f'Letras Erradas: {", ".join(letras_erradas)}')
+    
     palpite = input('\nDigite uma letra: ').upper()
 
     #Verifica se a letra está na palavra secreta
@@ -37,3 +40,9 @@ while tentativas > 0 and '_' in letras_descobertas:
         print(f"Oh, que pena! A Letra '{palpite}' não existe na palavra secreta. Tenta novamente!")
         letras_erradas.append(palpite)
         tentativas -= 1
+    print('|***||***| BEM-VINDO AO JOGO DA FORCA: VERSÃO UNIÃO EUROPEIA |***||***|')
+    print()
+    print('\n' + '='*40)
+    print(f'Palavra Secreta: {" ".join(letras_descobertas)}')
+    print(f'Vidas Restantes: {tentativas}')
+    print(f'Letras Erradas: {", ".join(letras_erradas)}')
